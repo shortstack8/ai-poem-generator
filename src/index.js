94932089aee1bbf381a0ac4d804ec09f1e342030
Tirelle P <tirelle.peter@icloud.com>
2025-04-15 18:55:57 +1000

    Added basic javascript form formatting and Typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem-body", {
+    strings: "Poem goes here",
+    autoStart: true,
+    delay: 50,
+    cursor: null,
+  });
+}
+
+let poemFormElement = document.getElementById("poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
